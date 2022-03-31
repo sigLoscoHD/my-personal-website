5ac347f6153745392ea31881f2eea6403b94e977
Matteo Cappella <matteo.cappella@leitha.eu>
2022-03-31 11:40:04 +0200

    Add personal info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderbarComponent } from './components/headerbar/headerbar.component';
 import { TranslateCompiler, TranslateModule } from '@ngx-translate/core';
+import { MenuComponent } from './components/menu/menu.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderbarComponent
+    HeaderbarComponent,
+    MenuComponent,
+    ProfileComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
